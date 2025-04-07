547872a4670c26bddfc729a76a121febd557c4c5
Timoti Fregni <timotif@gmail.com>
2025-04-07 12:12:58 +0200

    more resilient solver for level00

--- level00/resources/level00.py
+++ level00/resources/level00.py
@@ -2,9 +2,17 @@ from solver.utils import caesar_cipher, getflag
 
 def solve(connection) -> str | None:
 	flag00_files = connection.exec("find / -user flag00 2>/dev/null")
-	content = connection.exec(f"cat {flag00_files.split()[0]}")
-	flag_password = caesar_cipher(content, -15)
-	flag = getflag("00", flag_password)
-	if flag:
-		return flag_password, flag
+	for file in flag00_files.split():
+		try:
+			if file.startswith("/proc"):	# Skip /proc files
+				continue
+			content = connection.exec(f"cat {file} 2>/dev/null")
+			if not content or content.startswith("Error:"):
+				continue
+			flag_password = caesar_cipher(content, -15)
+			flag = getflag("00", flag_password)
+			if flag:
+				return flag_password, flag
+		except Exception as e:
+			continue
 	return None
